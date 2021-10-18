f62cc4f1760b895dfdb1bca5500b38ed16fa7216
Joao Paulo <jc652346@gmail.com>
2021-10-18 12:40:53 -0300

    Reusing intantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,15 +10,14 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department joao = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, joao);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(seller);
 		
-
+		System.out.println("=== TEST 1: seller findById ====");
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
+	
 	}
 
 }
